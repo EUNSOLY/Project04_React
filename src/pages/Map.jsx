ed52a53923f79594d1a715ae42d036716f53fbc4
EUNSOLY <dmsthf9596@gmail.com>
2023-04-27 11:37:25 +0900

    23.04.27 리액트 프로젝트 3일차

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -7,29 +7,31 @@ function Map() {
   let [y, setY] = useState(Number);
 
   let data = useSelector((state) => state.data);
+
   useEffect(() => {
     const doSomething = (x, y) => {
       setX(x);
       setY(y);
     };
 
+    // input 영역
+    const inputCon = document.querySelector(".");
+    // geolocation으로 구글로컬상의 현재 위치 생성
     navigator.geolocation.getCurrentPosition((position) => {
       doSomething(position.coords.latitude, position.coords.longitude);
     });
 
-    const container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
+    // 카카오맵 생성
+    const container = document.getElementById("map");
     const options = {
-      //지도를 생성할 때 필요한 기본 옵션
       center: new kakao.maps.LatLng(x, y), //지도의 중심좌표.
       level: 3, //지도의 레벨(확대, 축소 정도)
     };
     const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
 
-    // 마커 생성
-    // 지도에 표시된 마커 객체를 가지고 있을 배열입니다
-    let markers = [];
+    let markers = []; // 지도에 표시된 마커 객체를 가지고 있을 배열입니다
 
-    // 배열에 추가된 마커들을 지도에 표시하거나 삭제하는 함수입니다
+    // 데이터에 있는 배열들을 반복하면서 좌표값을 지도에 뿌려주기
     for (var i = 0; i < data.length; i++) {
       addMarker(
         new kakao.maps.LatLng(data[i].addX, data[i].addY),
@@ -43,26 +45,21 @@ function Map() {
         position: position,
         title: title,
       });
-      // 마커가 지도 위에 표시되도록 설정합니다
-      marker.setMap(map);
-      // 생성된 마커를 배열에 추가합니다
-      markers.push(marker);
 
-      // 마커에 커서가 오버됐을 때 마커 위에 표시할 인포윈도우를 생성합니다
+      marker.setMap(map); // 마커가 지도 위에 표시되도록 설정합니다
+      markers.push(marker); // 생성된 마커를 배열에 추가합니다
+
+      // 인포윈도우
       let iwContent = title;
-      // 인포윈도우를 생성합니다
       let infowindow = new kakao.maps.InfoWindow({
         content: iwContent,
       });
-      // 마커에 마우스오버 이벤트를 등록합니다
+
+      // 마커에 마우스오버 이벤트 (인포윈도우표시, 닫기)
       kakao.maps.event.addListener(marker, "mouseover", function () {
-        // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
         infowindow.open(map, marker);
       });
-
-      // 마커에 마우스아웃 이벤트를 등록합니다
       kakao.maps.event.addListener(marker, "mouseout", function () {
-        // 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
         infowindow.close();
       });
     }
